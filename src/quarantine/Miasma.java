db8f81fbbf3588397f023bb35546fb213d0c9fec
Zeale <zeale@dusttoash.org>
2020-03-07 13:35:34 -0600

    Add Main Menu stuff

--- src/quarantine/Miasma.java
+++ src/quarantine/Miasma.java
@@ -1,10 +1,24 @@
 package quarantine;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 
 public class Miasma {
 	public static void main(String[] args) {
+		Game.info().setName("Miasma");
+		Game.info().setVersion("v0b0-nightly");
+		Game.info().setDescription("An homage to the Corona virus.");
+
 		Game.init();
+
+		Game.graphics().setBaseRenderScale(2);
+		
+		Game.screens().add(new MainMenuScreen());
+
 		Game.start();
 	}
+}
+
+class TestScreen extends GameScreen {
+
 }
