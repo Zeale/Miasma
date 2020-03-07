47572406dc73308ffea1fab3f09dc268e8a39124
Audrey Cooper <audreygcooper@gmail.com>
2020-03-07 16:02:53 -0600

    update start map

--- rsc/tilesets/City 2.tsx
+++ rsc/tilesets/City 2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="City 2" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+<tileset version="1.2" tiledversion="1.3.3" name="City 2" tilewidth="400" tileheight="200" tilecount="37" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="tiles/GuttyKreumKanagawaItch/Tiles/Tile_268.png"/>
@@ -18,5 +18,98 @@
  </tile>
  <tile id="5">
   <image width="32" height="32" source="tiles/GuttyKreumKanagawaItch/Tiles/Tile_273.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="tiles/barrel32x32transparent.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="tiles/bush32x32transparent.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="tiles/bush32x32transparentanimated.gif"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="96" source="tiles/chopped96x96.gif"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="tiles/fence32x32transparent.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="tiles/flower32x32transparent.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="tiles/flower32x32transparentanimated.gif"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="tiles/grassa32x32transparent.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="tiles/grassb32x32transparent.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="tiles/grassc32x32transparent.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="tiles/grassd32x32transparent.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="tiles/grasse32x32transparent.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="tiles/grassf32x32transparent.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="tiles/grassg32x32transparent.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="tiles/grassh32x32transparent.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="tiles/grassi32x32transparent.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="tiles/patch32x32transparent.png"/>
+ </tile>
+ <tile id="23">
+  <image width="400" height="200" source="tiles/Preview1.gif"/>
+ </tile>
+ <tile id="24">
+  <image width="400" height="200" source="tiles/preview2.gif"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="tiles/riverbottom64x64transparentanimatednotbright.gif"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="tiles/riverbottomleft64x64.gif"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="tiles/rivertop64x64transparentanimated.gif"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="tiles/rivertopleft64x64transparent.gif"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="tiles/roadbottom32x32transparent.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="tiles/roadmiddle32x32transparent.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="tiles/roadtop32x32transparent.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="tiles/rock32x32transparent.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="tiles/signpost32x32transparent.png"/>
+ </tile>
+ <tile id="34">
+  <image width="400" height="200" source="tiles/Tilesetpreview1.gif"/>
+ </tile>
+ <tile id="35">
+  <image width="96" height="96" source="tiles/tree96x96transparent.png"/>
+ </tile>
+ <tile id="36">
+  <image width="96" height="96" source="tiles/tree96x96transparentanimated.gif"/>
  </tile>
 </tileset>
