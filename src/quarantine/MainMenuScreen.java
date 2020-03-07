7d9b27028d3b8fb4502e6b34a928159d814fa38b
Zeale <zeale@dusttoash.org>
2020-03-07 14:25:59 -0600

    Text is rendered

--- src/quarantine/MainMenuScreen.java
+++ src/quarantine/MainMenuScreen.java
@@ -1,18 +1,15 @@
 package quarantine;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.environment.Environment;
+import de.gurkenlabs.litiengine.graphics.TextRenderer;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.gurkenlabs.litiengine.resources.Resources;
 
 public class MainMenuScreen extends Screen {
-
-	private final Environment env = Game.world().loadEnvironment("rsc/tilemaps/Start.tmx");
-	{
-		env.getMap().getTilesets().add(Resources.tilesets().get("rsc/tilesets/Miasma City-1.tsx"));
-	}
 
 	protected MainMenuScreen() {
 		super("main-menu");
@@ -21,6 +18,11 @@ public class MainMenuScreen extends Screen {
 	@Override
 	public void render(Graphics2D g) {
 		super.render(g);
+		g.setFont(Resources.fonts().get("rsc/fonts/Pixel LCD-7.ttf", 72f));
+		g.setColor(Color.RED);
+		Point2D center = Game.window().getCenter();
+		double x = center.getX(), y = center.getY() / 3 * 2;
+		TextRenderer.render(g, "MIASMA", x, y);
 	}
 
 }
