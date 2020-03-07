fd27ea9a30ae416b13687149c4f443f996e0bfd8
Audrey Cooper <audreygcooper@gmail.com>
2020-03-07 17:06:49 -0600

    Remove garbage

--- rsc/tilemaps/City 3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="City 3" tilewidth="32" tileheight="32" tilecount="72" columns="12">
- <image source="../tilesets/tiles/Tilesetpreview1.gif" width="400" height="200"/>
-</tileset>
