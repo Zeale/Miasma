74c26bcc6b0096e672227d04c54b588bbcaa0217
Zeale <zeale@dusttoash.org>
2020-03-07 21:58:12 -0600

    Got sprites to finally load

--- src/quarantine/Character.java
+++ src/quarantine/Character.java
@@ -1,13 +1,14 @@
 package quarantine;
 
 import de.gurkenlabs.litiengine.entities.Creature;
+import de.gurkenlabs.litiengine.resources.Resources;
 
 public class Character extends Creature {
 
 	public Character() {
 		super("Mega-Shlong");
-		setSpritePrefix("rsc/Sprites/MegaMan.png");
-
+		Resources.spritesheets().loadFrom("rsc/sprites/sprites.info");
+		setSpritePrefix("megaman");
 	}
 
 }
