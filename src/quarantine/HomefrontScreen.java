9369b6f84bd05d8394c6259c491478447779e7b8
Zeale <zeale@dusttoash.org>
2020-03-07 19:36:26 -0600

    Set up time

--- src/quarantine/HomefrontScreen.java
+++ src/quarantine/HomefrontScreen.java
@@ -11,7 +11,6 @@ import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 public class HomefrontScreen extends GameScreen {
 	private final Environment environment = Game.world().loadEnvironment("rsc/tilemaps/Start.tmx");
 	private final AmbientLight lighting = new AmbientLight(environment, Color.blue);
-
 	{
 		setNight(false);
 	}
@@ -20,9 +19,13 @@ public class HomefrontScreen extends GameScreen {
 		lighting.setAlpha(night ? 50 : 0);
 	}
 
+	private boolean t = false;
+
 	@Override
 	public void render(Graphics2D g) {
 		super.render(g);
+		if (t ^ (t = ((Game.loop().getTicks() / Game.loop().getTickRate()) / 150 & 1) == 1))
+			setNight(t);
 		lighting.render(g);
 	}
 
