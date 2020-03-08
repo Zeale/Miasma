3fe87ead2b72b902009bfd3e0d8f9fbb2ec5998e
Nathan Hoogendam <nhoogendam@gmail.com>
2020-03-07 20:43:20 -0600

    bAOop

---
+++ src/quarantine/Character.java
@@ -0,0 +1,13 @@
+package quarantine;
+
+import de.gurkenlabs.litiengine.entities.Creature;
+
+public class Character extends Creature {
+
+	public Character() {
+		super("Mega-Shlong");
+		setSpritePrefix("rsc/Sprites/MegaMan.png");
+
+	}
+
+}
--- src/quarantine/HomefrontScreen.java
+++ src/quarantine/HomefrontScreen.java
@@ -6,9 +6,12 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.entities.Entity;
+import de.gurkenlabs.litiengine.entities.Spawnpoint;
 import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.graphics.AmbientLight;
 import de.gurkenlabs.litiengine.graphics.TextRenderer;
+import de.gurkenlabs.litiengine.gui.DropdownListField;
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 import de.gurkenlabs.litiengine.resources.Resources;
 
@@ -16,7 +19,23 @@ public class HomefrontScreen extends GameScreen {
 	private static final Font DAY_COUNT_FONT = Resources.fonts().get("rsc/fonts/Pixel LCD-7.ttf", 32f);
 	private final Environment environment = Game.world().loadEnvironment("rsc/tilemaps/Start.tmx");
 	private final AmbientLight lighting = new AmbientLight(environment, Color.blue);
+	private final Entity nathanIsAMegaShlong = new Character();
 	{
+
+		String[] content = { "Yorick loves Java", "Audrey is cool", "Dillon is awesome", "Nathan is the coolest" };
+		DropdownListField dropDown = new DropdownListField(50, 30, 250, 250, content, content.length);
+		dropDown.setHeight(150);
+		getComponents().add(dropDown);
+
+		DropdownListField dropDown2 = new DropdownListField(350, 30, 250, 250, content, content.length);
+		dropDown2.setHeight(150);
+		getComponents().add(dropDown2);
+
+		nathanIsAMegaShlong.setVisible(true);
+		environment.add(nathanIsAMegaShlong);
+		Spawnpoint enter = environment.getSpawnpoint("hospital");
+		if (enter != null)
+			enter.spawn(nathanIsAMegaShlong);
 		setNight(false);
 	}
 
