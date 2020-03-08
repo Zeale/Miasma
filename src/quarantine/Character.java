eba98bb3f124a876d2f74340e56b08d11db3f276
Zeale <zeale@dusttoash.org>
2020-03-07 23:00:16 -0600

    Add goTo meth in Character cls

--- src/quarantine/Character.java
+++ src/quarantine/Character.java
@@ -1,9 +1,20 @@
 package quarantine;
 
+import java.awt.geom.Point2D;
+
+import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.entities.Creature;
+import de.gurkenlabs.litiengine.pathfinding.EntityNavigator;
+import de.gurkenlabs.litiengine.pathfinding.astar.AStarPathFinder;
 import de.gurkenlabs.litiengine.resources.Resources;
 
 public class Character extends Creature {
+	private final EntityNavigator en = new EntityNavigator(this,
+			new AStarPathFinder(Game.world().environment().getMap()));
+
+	public void goTo(Point2D point) {
+		en.navigate(point);
+	}
 
 	public Character() {
 		super("Mega-Shlong");
